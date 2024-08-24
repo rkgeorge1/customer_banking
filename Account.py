c0e23b789a58667a7fcf784d4d2428c75c6e2d91
rkgeorge1 <165116512+rkgeorge1@users.noreply.github.com>
2024-08-24 12:05:42 -0700

    Add files via upload

---
+++ Account.py
@@ -0,0 +1,26 @@
+""" Create a Account class with methods"""
+
+class Account:
+    """Creating an Account class with methods"""
+    def __init__(self, balance, interest):
+        self.balance = balance
+        self.interest = interest
+
+    # This method sets the balance of the account.
+    def set_balance(self, balance):
+        """Sets the balance for the for the account"""
+        self.balance = balance
+
+    # The method sets the interest gained for the account.
+    def set_interest(self, interest):
+        """Sets the interest gained for the the account"""
+        self.interest = interest
+
+    def get_balance(self):
+        """Returns the current balance of the account."""
+        return self.balance
+
+    def get_interest(self):
+        """Returns the current interest earned on the account."""
+        return self.interest
+
